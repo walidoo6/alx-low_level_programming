744b215a9ee1c2251f34da33124db160f0615fef
walidoo6 <w.ouhida@mundiapolis.ma>
2023-03-16 08:18:03 -0700

    xx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(){
 	printf("Size of a int: %lu byte(s)\n",(unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
-	printf("Size of a floqtt: %lu byte(s)\n",(unsigned long)sizeof(e));
+	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(e));
 	return 0;
 }
